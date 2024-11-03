a0e1feebc1292bc6be4e9e3366dd0679c6a32350
freightant <tech@freightant.com>
2024-11-03 15:41:55 +0530

    DB ISSUE

--- src/app/api/location/locode/route.ts
+++ src/app/api/location/locode/route.ts
@@ -1,21 +1,52 @@
-import pool from "@/network/db/connection";
+import{ sql } from "@/network/db/connection";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
       let name:any = await request.nextUrl.searchParams
       let id:any = await request.nextUrl.searchParams
-      const connection = await pool.getConnection();
+      let str:string = name?.get("name")?name?.get("name") :""
       let query = "";
       if(name.get("name")){
-        query = "SELECT DISTINCT `t1`.`id`, `t1`.`Country`, `t1`.`Name`,`t1`.`Location`,`t1`.`Subdivision`,`t2`.`emoji`,`t2`.`currency`, `t2`.`name` AS `countryname`,`t3`.`name` AS `statename`, `t1`.`FullName` FROM `locode` AS `t1` INNER JOIN `countries` AS `t2` ON `t2`.`iso2` = `t1`.`Country` LEFT JOIN `states` AS `t3` ON `t3`.`iso2` = `t1`.`Subdivision` WHERE `t1`.`Location` LIKE '%"+name.get("name")+"%' OR `t1`.`Country` LIKE '%"+name.get("name")+"%' OR `t1`.`Name` LIKE '%"+name.get("name")+"%' OR `t1`.`FullName` LIKE '%"+name.get('name')+"%' GROUP BY `t1`.`id`, `t1`.`Country`, `t1`.`Name`, `t1`.`Location`, `t1`.`Subdivision`, `t2`.`emoji`, `t2`.`currency`, `t2`.`name`, `t1`.`FullName` LIMIT 50";
+        query = "";
       }
       if(name.get("id")){
         query = "SELECT DISTINCT `t1`.`id`, `t1`.`Country`, `t1`.`Name`,`t1`.`Location`,`t1`.`Subdivision`,`t2`.`emoji`,`t2`.`currency`, `t2`.`name` AS `countryname`,`t3`.`name` AS `statename`, `t1`.`FullName` FROM `locode` AS `t1` INNER JOIN `countries` AS `t2` ON `t2`.`iso2` = `t1`.`Country` LEFT JOIN `states` AS `t3` ON `t3`.`iso2` = `t1`.`Subdivision` WHERE `t1`.`id` = "+id.get("id");
       }
-      const [rows] = await connection.execute(query);
-      connection.release();
-      return NextResponse.json({code:true, data:rows});
+      const res = await sql`
+select distinct
+  t1.id,
+  t1."Country",
+  t1."Name",
+  t1."Location",
+  t1."Subdivision",
+  t2.emoji,
+  t2.currency,
+  t2.name as countryname,
+  ${sql`COALESCE(MAX(t3.name), '') AS statename`},
+  t1."FullName"
+from
+  locode as t1
+  inner join countries as t2 on t2.iso2 = t1."Country"
+  left join states as t3 on t3.iso2 = t1."Subdivision"
+where
+  t1."Location" ilike ${"%"+str + "%"}
+  or t1."Country" ilike ${"%"+str + "%"}
+  or t1."Name" ilike ${"%"+str + "%"}
+  or t1."FullName" ilike ${"%"+str + "%"}
+group by
+  t1.id,
+  t1."Country",
+  t1."Name",
+  t1."Location",
+  t1."Subdivision",
+  t2.emoji,
+  t2.currency,
+  t2.name,
+  t1."FullName"
+limit
+  50;`
+      return NextResponse.json({code:true, data:res});
     } catch (error) {
       console.error(error);
       return NextResponse.json({code:false, error: 'Database error' });
